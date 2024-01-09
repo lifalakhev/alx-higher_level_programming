15ef9a69282e1e0248e4b4dd35b2e37830c3ed41
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2024-01-08 23:29:19 -0800

    python input and output project

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns:
+        str: JSON representation of object.
+    """
+    # print("type json.dumps(my_obj)--> {}".format(type(json.dumps(my_obj))))
+    # print("type my_obj--> {}".format(type(my_obj)))
+
+    # serializing json
+    return json.dumps(my_obj)
